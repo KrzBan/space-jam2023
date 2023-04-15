f7e1e3db7bb14fd57891a19af42ebc6e3771b9b8
Krzysztof Bański <krzys0100@wp.pl>
2023-04-15 22:48:35 +0200

    Sparks

--- SpaceJam2023/Assets/Bullet.cs
+++ SpaceJam2023/Assets/Bullet.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float damage = 50;
- 
+    public ParticleSystem sparks;
+
     void Awake()
     {
         Destroy(gameObject, damage);
@@ -16,6 +17,12 @@ public class Bullet : MonoBehaviour
         if(collision.gameObject.TryGetComponent<EnemyController>(out EnemyController enemyComponent)){
             enemyComponent.TakeDamage(damage);
         }
+
+        var contactPoint = collision.GetContact(0);
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contactPoint.normal);
+        var newSparks = Instantiate(sparks, contactPoint.point, rot) as ParticleSystem;
+        newSparks.Play();
+
         Destroy(gameObject);
     }
 }
--- SpaceJam2023/Assets/EnemyBullet.cs
+++ SpaceJam2023/Assets/EnemyBullet.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
     public float damage = 5;
- 
+    public ParticleSystem sparks;
+
     void Awake()
     {
         Destroy(gameObject, damage);
@@ -15,6 +16,12 @@ public class EnemyBullet : MonoBehaviour
     {
         if(collision.gameObject.TryGetComponent<PlayerController>(out PlayerController playerComponent)){
             playerComponent.TakeDamage(damage);
+
+            var contactPoint = collision.GetContact(0);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contactPoint.normal);
+            var newSparks = Instantiate(sparks, contactPoint.point, rot) as ParticleSystem;
+            newSparks.Play();
+
             Destroy(gameObject);
         }
     }
