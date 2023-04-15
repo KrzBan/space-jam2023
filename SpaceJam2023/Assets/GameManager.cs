561df2d8a92e00d7282213a4797f10783f8dcafc
pact0 <kuba.kowalski21331@gmail.com>
2023-04-15 19:41:48 +0200

    track number of enemies

--- SpaceJam2023/Assets/GameManager.cs
+++ SpaceJam2023/Assets/GameManager.cs
@@ -7,9 +7,28 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject playerSpaceship;
 
+    public List<EnemyController> enemies = new List<EnemyController>();
+
     void Awake()
     {
+        //enemies = GameObject.FindObjectsOfType<EnemyController>().ToList();
+        enemies = new List<EnemyController>();
+        enemies.Add(GameObject.FindObjectOfType<EnemyController>());
         instance = this;
+    }
+
+    void OnEnable()
+    {
+        EnemyController.OnEnemyKilled += HandleEnemyKilled;
+    }
+
+    void OnDisable()
+    {
+        EnemyController.OnEnemyKilled -= HandleEnemyKilled;
+    }
+
+    void HandleEnemyKilled(EnemyController enemy){
+        // enemies.Remove(enemy);
     }
 
 }
